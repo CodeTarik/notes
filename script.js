2ba4670c7efbbf8aeeb39d02304475cd1e0b7ff7
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-09-09 01:10:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+    let notes = ['banana', 'döner kaufen'];
+
+    function renderNotes() {
+    //I must define where they are to be displayed
+    let contenRef = document.getElementById('content');
+    contenRef.innerHTML = "";
+    
+    for (let indexNote = 0; indexNote < notes.length; indexNote++){
+        const note = notes[indexNote];
+        contenRef.innerHTML += getNoteTemplate(note);
+    }
+    }
+
+    function getNoteTemplate(note){
+        return `<p>+ ${note}</p>`;
+    }
+
+
+//notizen hinzufügen
+function addNote(){
+    let noteInputRef = document.getElementById('note_input');
+    let noteInput = noteInputRef.value;
+
+    notes.push(noteInput);
+    renderNotes();
+
+    //Delete or clear again after entering the note
+    noteInputRef.value = ''; 
+}
+
+//notizen löschen
+
+function deleteNote(){
+    let noteDeleteRef = document.getElementById('note_input');
+    let noteDelete = noteDeleteRef.value;
+
+    notes.pull(noteDelete);
+    renderNotes();
+}
+
+
+//notizen archivieren
